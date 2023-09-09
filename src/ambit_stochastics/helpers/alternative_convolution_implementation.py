be9f36f596574eb3da065c248f3b2829a90be044
Dan Leonte <dl2916@ic.ac.uk>
2023-09-09 21:18:13 +0100

    added a test
    added a test case

--- src/ambit_stochastics/helpers/alternative_convolution_implementation.py
+++ src/ambit_stochastics/helpers/alternative_convolution_implementation.py
@@ -3,3 +3,7 @@ def cumulative_and_diagonal_sums(a):
     k = a.shape[-1] #nr_trawls
     a = np.cumsum(a[::-1],axis=0)[::-1]
     return np.bincount(sum(np.indices(a.shape)).flat, a.flat)[:k]
+
+
+#a = np.array([[0,1,0,1,1],[-1,0,1,2,1],[1,0,2,1,2],[1,0,2,1,2],[1,0,2,1,2]])
+#cumulative_and_diagonal_sums(a)
